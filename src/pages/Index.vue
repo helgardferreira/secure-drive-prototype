8161529df9c8035e0145db7dbcfc295cb140f906
Helgard Ferreira <hrfer3@gmail.com>
2019-04-21 21:00:00 +0200

    Begin Home Page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,25 @@
 <template>
-  <q-page class="flex flex-center"></q-page>
+  <q-page>
+    <div class="q-pa-md">
+      <div class="row">
+        <div class="col text-center">
+          <h4>SECUREDRIVE</h4>
+        </div>
+      </div>
+      <div class="row menu">
+        <div class="col text-center">Make Booking</div>
+        <div class="col text-center">Make Payment</div>
+      </div>
+      <div class="row menu">
+        <div class="col text-center">Rate Our Services</div>
+      </div>
+      <div class="row menu">
+        <div class="col text-center">Update Profile</div>
+        <div class="col text-center">Enquiry</div>
+        <div class="col text-center">Call Now</div>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <style>
@@ -7,6 +27,15 @@
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+
+  methods: {
+    test() {
+      console.log('test')
+    }
+  }
 }
 </script>
+
+<style>
+</style>

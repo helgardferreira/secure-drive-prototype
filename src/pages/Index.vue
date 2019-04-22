bfb8ec9be0bdbf914b419998d033f71705bbed9d
Helgard Ferreira <hrfer3@gmail.com>
2019-04-22 08:35:52 +0200

    Prototype Flexbox Buttons

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,40 @@
 <template>
   <q-page>
-    <div class="q-pa-md">
-      <div class="row">
+    <div class="column">
+      <div class="col row justify-around">
         <div class="col text-center">
           <h4>SECUREDRIVE</h4>
         </div>
       </div>
-      <div class="row menu">
-        <div class="col text-center">Make Booking</div>
-        <div class="col text-center">Make Payment</div>
+
+      <div class="col row justify-around">
+        <div class="col text-center">
+          <q-btn round label="Make Booking" color="primary" class="menu-btn"/>
+        </div>
+
+        <div class="col text-center">
+          <q-btn round label="Make Payment" color="primary" class="menu-btn"/>
+        </div>
       </div>
-      <div class="row menu">
-        <div class="col text-center">Rate Our Services</div>
+
+      <div class="col row justify-around">
+        <div class="col text-center">
+          <q-btn round label="Rate Our Services" color="primary" class="menu-btn"/>
+        </div>
       </div>
-      <div class="row menu">
-        <div class="col text-center">Update Profile</div>
-        <div class="col text-center">Enquiry</div>
-        <div class="col text-center">Call Now</div>
+
+      <div class="col row justify-around">
+        <div class="col text-center">
+          <q-btn round label="Update Profile" color="primary" class="menu-btn"/>
+        </div>
+
+        <div class="col text-center">
+          <q-btn round label="Enquiry" color="primary" class="menu-btn"/>
+        </div>
+
+        <div class="col text-center">
+          <q-btn round label="Call Now" color="primary" class="menu-btn"/>
+        </div>
       </div>
     </div>
   </q-page>
@@ -37,5 +55,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.column {
+  min-height: 450px;
+  height: calc(100vh - 50px);
+}
+
+.menu-btn {
+  font-size: calc(14px + (26 - 14) * ((100vw - 300px) / (1600 - 300)));
+  min-width: 6em;
+  min-height: 6em;
+}
 </style>
